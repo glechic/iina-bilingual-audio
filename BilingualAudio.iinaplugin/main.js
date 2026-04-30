508e03dcd61abdb13a7e8589e1aa1eb8ebc41faa
Sergey Pashkevich <serg.pash99@gmail.com>
2026-04-30 16:30:36 +0200

    Fix: Move message handlers from global.js to main.js using sidebar object

--- BilingualAudio.iinaplugin/main.js
+++ BilingualAudio.iinaplugin/main.js
@@ -1,5 +1,48 @@
 const { core, mpv, event, sidebar } = iina;
 
+sidebar.onMessage('test-message', () => {
+  sidebar.postMessage('mix-result', {
+    success: true,
+    message: 'Test received! global.js working.'
+  });
+});
+
+sidebar.onMessage('apply-mix', (data) => {
+  sidebar.postMessage('mix-result', {
+    success: true,
+    message: 'Applying: ' + data.mode + ' (t1=' + data.track1Id + ', t2=' + data.track2Id + ')'
+  });
+  
+  let filter = '';
+  
+  if (data.mode === 'stereo') {
+    filter = '[aid' + data.track1Id + ']pan=stereo|c0=' + data.vol1 + '*c0|c1=0[left];' +
+             '[aid' + data.track2Id + ']pan=stereo|c0=0|c1=' + data.vol2 + '*c0[right];' +
+             '[left][right]amix=inputs=2[ao]';
+  } else if (data.mode === 'mixed') {
+    filter = '[aid' + data.track1Id + ']volume=' + data.vol1 + '[a1];' +
+             '[aid' + data.track2Id + ']volume=' + data.vol2 + '[a2];' +
+             '[a1][a2]amix=inputs=2:duration=longest[ao]';
+  }
+  
+  if (filter) {
+    mpv.setProperty('lavfi-complex', filter);
+    sidebar.postMessage('mix-result', {
+      success: true,
+      message: 'Filter applied: ' + data.mode
+    });
+  } else {
+    mpv.setProperty('lavfi-complex', '');
+    core.audio.id = data.track1Id;
+    sidebar.postMessage('mix-result', { success: true, message: 'Single track mode' });
+  }
+  
+  const pos = mpv.getProperty('time-pos');
+  if (pos !== null && pos !== undefined) {
+    mpv.command('seek', pos, 'absolute');
+  }
+});
+
 event.on('mpv.file-loaded', () => {
   setTimeout(() => {
     let audioTracks = core.audio?.tracks || [];
