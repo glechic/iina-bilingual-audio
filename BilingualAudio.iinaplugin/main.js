38ab62b54bbdd1342713700613876251ff580f7b
Sergey Pashkevich <serg.pash99@gmail.com>
2026-06-19 12:16:01 +0200

    Refactor: clean up code, simplify sidebar UI, adopt IINA native styling
    - Extract helpers (buildBilingualFilter, enableBilingual, disableBilingual, getAudioTracks)
    - Simplify sidebar to toggle + two dropdowns, no custom styles
    - Adopt IINA opensub styling conventions (color-scheme, prefers-color-scheme, CSS variables)
    - Simplify preferences.html to native data-pref-key pattern
    - Wire auto_show preference to sidebar.show()
    - Remove unused dead code and debug readback

--- BilingualAudio.iinaplugin/main.js
+++ BilingualAudio.iinaplugin/main.js
@@ -1,74 +1,64 @@
-const { core, mpv, sidebar, menu, event } = iina;
+const { core, mpv, sidebar, menu, event, preferences } = iina;
 
 console.log('Audio Mixer plugin loaded');
 
 menu.addItem(
-  menu.item('Show Audio Mixer', () => {
-    sidebar.show();
-  })
+  menu.item('Show Audio Mixer', () => sidebar.show())
 );
 
+function buildBilingualFilter(t1, t2) {
+  return '[aid' + t1 + ']aformat=channel_layouts=mono[mono1];' +
+         '[aid' + t2 + ']aformat=channel_layouts=mono[mono2];' +
+         '[mono1][mono2]amerge=inputs=2[ao]';
+}
+
+function disableBilingual(trackId) {
+  mpv.set('lavfi-complex', '');
+  mpv.set('aid', trackId);
+}
+
+function enableBilingual(t1, t2) {
+  mpv.set('aid', 'no');
+  mpv.set('lavfi-complex', buildBilingualFilter(t1, t2));
+}
+
 event.on('iina.window-loaded', () => {
-  console.log('Window loaded, loading sidebar');
   sidebar.loadFile('sidebar.html');
-  
+
   sidebar.onMessage('apply-mix', (data) => {
-    console.log('APPLY RECEIVED:', data);
-
     try {
-      if (!data.enabled) {
-        mpv.set('lavfi-complex', '');
-        mpv.set('aid', data.track1Id);
+      if (data.enabled) {
+        enableBilingual(data.track1Id, data.track2Id);
+        sidebar.postMessage('mix-result', { success: true, message: 'Bilingual on' });
+      } else {
+        disableBilingual(data.track1Id);
         sidebar.postMessage('mix-result', { success: true, message: 'Bilingual off' });
-        return;
       }
-
-      mpv.set('aid', 'no');
-
-      const t1 = data.track1Id;
-      const t2 = data.track2Id;
-      const filter = '[aid' + t1 + ']aformat=channel_layouts=mono[mono1];' +
-                     '[aid' + t2 + ']aformat=channel_layouts=mono[mono2];' +
-                     '[mono1][mono2]amerge=inputs=2[ao]';
-      console.log('Filter:', filter);
-
-      mpv.set('lavfi-complex', filter);
-
-      const readback = mpv.getString('lavfi-complex');
-      const aid = mpv.getString('aid');
-      console.log('Readback:', readback, 'aid:', aid);
-      sidebar.postMessage('mix-result', { success: true, message: 'On (aid=' + aid + '): ' + (readback || '').slice(0, 40) });
     } catch (e) {
       sidebar.postMessage('mix-result', { success: false, message: 'Error: ' + e });
     }
   });
 });
 
-event.on('mpv.file-loaded', () => {
-  console.log('File loaded');
-  
-  setTimeout(() => {
-    let audioTracks = core.audio?.tracks || [];
-    
-    if (audioTracks.length === 0) {
-      const trackList = mpv.getNative('track-list');
-      if (trackList) {
-        audioTracks = trackList.filter(t => t.type === 'audio');
-      }
+function getAudioTracks() {
+  let tracks = core.audio?.tracks || [];
+  if (tracks.length === 0) {
+    const trackList = mpv.getNative('track-list');
+    if (trackList) {
+      tracks = trackList.filter(t => t.type === 'audio');
     }
-    
-    console.log('Audio tracks:', audioTracks.length);
-    
+  }
+  return tracks;
+}
+
+event.on('mpv.file-loaded', () => {
+  setTimeout(() => {
+    const audioTracks = getAudioTracks();
     if (audioTracks.length > 1) {
-      console.log('Sending tracks-loaded');
-      sidebar.postMessage('tracks-loaded', {
-        tracks: audioTracks,
-        mode: 'single',
-        vol1: 1.0,
-        vol2: 1.0
-      });
+      sidebar.postMessage('tracks-loaded', { tracks: audioTracks });
+      if (preferences.get('auto_show')) {
+        sidebar.show();
+      }
     }
   }, 1000);
 });
-
-
